fcbf6c66a73361db53da1515ba06f37bbac56ec1
rohl17-1 <209197994+rohl17-1@users.noreply.github.com>
2025-12-28 00:08:32 -0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -4,7 +4,7 @@ export const INITIAL_DATA: DataSource[] = [
   {
     "id": 1,
     "category": "email",
-    "origem": "K17 - Kalias",
+    "origem": "Apenas Teste-01",
     "dica": "Utilize o código do chamado (ex: 9999).",
     "email": "email01@gmail.com",
     "email2": "email02@gmail.com",
@@ -20,7 +20,7 @@ export const INITIAL_DATA: DataSource[] = [
   {
     "id": 2,
     "category": "email",
-    "origem": "SL-Apoio",
+    "origem": "Apenas Teste-02",
     "dica": "Prefixo de setor necessário (ex: FIN-).",
     "email": "support@gmail.com",
     "email2": "billing@gmail.com",
@@ -36,21 +36,56 @@ export const INITIAL_DATA: DataSource[] = [
   {
     "id": 3,
     "category": "email",
-    "origem": "Dev-Team",
-    "email": "dev.lead@test.com",
-    "email2": "dev.frontend@test.com",
-    "email3": "dev.backend@test.com"
+    "origem": "K*7 - Kalias",
+    "dica": "Utilize o código do chamado (ex: 9999).",
+    "email": "contaapp@cod.aleeas.com",
+    "email2": "emailtempor@cod.slmail.me",
+    "email3": "emailtemporario@cod.aleeas.com",
+    "email4": "redesocial@cod.aleeas.com",
+    "email5": "redesocial01@cod.aleeas.com",
+    "email6": "lixo@cod.slmail.me",
+    "email7": "kowalski@cod.aleeas.com",
+    "email8": "capitao@cod.aleeas.com",
+    "email9": "recruta@cod.aleeas.com",
+    "email10": "rico@cod.aleeas.com"
+  },
+  {
+    "id": 3,
+    "category": "email",
+    "origem": "SL-Apoio",
+    "dica": "Utilize o código do chamado (ex: 9999).",
+    "email": "tiktokconta@vintage.aleeas.com",
+    "email2": "conta02@falaserio.slmail.me",
+    "email3": "conta03@vintage.aleeas.com",
+    "email4": "conta04@kanonimo.slmail.me",
+    "email5": "conta05@astuto.aleeas.com",
+    "email6": "conta06@invadindo.aleeas.com",
+    "email7": "rico@vintage.aleeas.com",
+    "email8": "sotestando@vintage.aleeas.com",
+    "email9": "conta01@vintage.aleeas.com",
+    "email10": "audaz@astuto.aleeas.com"
   },
   {
     "id": 7,
     "category": "document",
     "origem": "Documentos",
     "items": [
-      { "type": "CPF", "number": "248.000.724-33" },
+      { "type": "CPF", "number": "427.132.390-01" },
       { "type": "RG", "number": "12.345.678-9" },
-      { "type": "CNPJ", "number": "12.345.678/0001-90" },
-      { "type": "CNPJ", "number": "17.345.678/0001-90" },
-      { "type": "CNH", "number": "12345678900" }
+      { "type": "CNPJ", "number": "25.302.775/0001-42" },
+      { "type": "CNH", "number": "19.746.500/0001-03" },
+      { "type": "TEL", "number": "(71)98369-5568" },
+      { "type": "CPF", "number": "22497072227" },
+      { "type": "RG", "number": "47.677.462-7" },
+      { "type": "CNPJ", "number": "39.733.272/0001-36" },
+      { "type": "CNH", "number": "56356064578" },
+      { "type": "TELL", "number": "(21) 98336-9514" },
+      { "type": "CPF", "number": "021,754,317-03" },
+      { "type": "RG", "number": "12.345.678/0001-90" },
+      { "type": "CNPJ", "number": "37.202.552/0001-92" },
+      { "type": "CNH", "number": "00598161653" },
+      { "type": "CNH", "Site": "https://www.4devs.com.br/gerador_de_pessoas"},
+      { "type": "CNH", "Site": "https://geradornv.com.br/gerador-celular/"}
     ]
   },
   {
@@ -58,10 +93,15 @@ export const INITIAL_DATA: DataSource[] = [
     "category": "cep",
     "origem": "Endereços (CEPs)",
     "items": [
-      { "dd": "27", "number": "29100-000" },
-      { "dd": "11", "number": "01001-000" },
+      { "dd": "21", "number": "23042-110" },
+      { "dd": "11", "number": "03359-025" },
       { "dd": "21", "number": "20040-002" },
-      { "dd": "31", "number": "30140-071" }
+      { "dd": "73", "number": "44046-000" },
+      { "dd": "87", "number": "54100-656" },
+      { "dd": "37", "number": "38184-114" },
+      { "dd": "21", "number": "21735-210" },
+      { "dd": "27", "number": "29172-680" },
+      { "dd": "73", "number": "45830-408" }
     ]
   }
 ];
